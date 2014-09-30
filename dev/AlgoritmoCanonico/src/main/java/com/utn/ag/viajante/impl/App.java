c3dc1210450eead84dbea53cbe3f6afd35e859e0
estcap <esebancaparroz@hotmail.com>
2014-09-30 16:08:08 -0300

    Empezado el Genético. Está todo ROTO y no funciona.

--- dev/AlgoritmoCanonico/src/main/java/com/utn/ag/viajante/impl/App.java
+++ dev/AlgoritmoCanonico/src/main/java/com/utn/ag/viajante/impl/App.java
@@ -1,12 +1,20 @@
 package com.utn.ag.viajante.impl;
 
+import com.utn.ag.algoritmocanonico.AppConstants;
+import com.utn.ag.algoritmocanonico.MockedLogger;
+import com.utn.ag.algoritmocanonico.vo.InformeVO;
 import com.utn.ag.viajante.model.Constants;
+import com.utn.ag.viajante.model.Poblacion;
+
 import java.io.IOException;
 
 public class App {
 
     public static void main(String[] args) throws IOException {
         HeuristicaFacu.DEBUG = Boolean.FALSE;
+       
+        System.out.println("Algoritmo Heurístico:");
+        
         int mejorCiudad = -1;
         int mejorRecorrido = -1;
         for (int i = 0; i < Constants.CANTIDAD_PROVINCIAS; i++) {
@@ -20,6 +28,19 @@ public class App {
             hf.printCiudadesRecorridas();
         }
         System.out.println("Mejor ciudad: " + mejorCiudad + " - " + Constants.NOMBRES_PROVINCIAS[mejorCiudad] + " km recorridos: " + mejorRecorrido);
-    }
+  
+    
+    
+    
+    System.out.println( "\n\nAlgoritmo Genético:");
+    
+   
+	
+    
+    
+    
+  
+
+}
 
 }
